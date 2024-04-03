06068652d6be7238d7121a3675383f1e81788032
pat-alt <altmeyerpat@gmail.com>
2024-04-03 17:51:08 +0200

    now properly extending methdos

--- ext/MPIExt/generate_counterfactual.jl
+++ ext/MPIExt/generate_counterfactual.jl
@@ -12,7 +12,7 @@ using Serialization
 
 Parallelizes the `CounterfactualExplanations.generate_counterfactual` function using `MPI.jl`. This function is used to generate counterfactual explanations.
 """
-function parallelize(
+function TaijaParallel.parallelize(
     parallelizer::MPIParallelizer,
     f::typeof(CounterfactualExplanations.generate_counterfactual),
     args...;
