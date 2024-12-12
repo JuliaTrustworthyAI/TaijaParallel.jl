56809193f56bfc83dadc31065cf482a333f68e48
pat-alt <altmeyerpat@gmail.com>
2024-12-12 17:27:37 +0100

    lord have mercy

--- ext/MPIExt/generate_counterfactual.jl
+++ ext/MPIExt/generate_counterfactual.jl
@@ -46,10 +46,11 @@ function TaijaBase.parallelize(
 
     # For each chunk:
     for (i, chunk) in enumerate(chunks)
+        @info "Rank $(parallelizer.rank): Number elements in `chunk`: $(length(chunk))."
         worker_chunk = TaijaParallel.split_obs(chunk, parallelizer.n_proc)
         @info "Rank $(parallelizer.rank): Number elements in `worker_chunk`: $(length(worker_chunk)). Scattering ..."
         worker_chunk = MPI.scatter(worker_chunk, parallelizer.comm)
-        @info "Generating ..."
+        @info "Rank $(parallelizer.rank): Generating ..."
         if !isempty(worker_chunk)
             worker_chunk = stack(worker_chunk; dims = 1)
             if !parallelizer.threaded
