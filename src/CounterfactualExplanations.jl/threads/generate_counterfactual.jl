68519485e085666a5eda603f17fc620f00bf1c02
pat-alt <altmeyerpat@gmail.com>
2025-01-01 11:34:20 +0100

    done

--- src/CounterfactualExplanations.jl/threads/generate_counterfactual.jl
+++ src/CounterfactualExplanations.jl/threads/generate_counterfactual.jl
@@ -29,7 +29,12 @@ function TaijaBase.parallelize(
     args = zip(counterfactuals, target, data, M, generator)
 
     # Preallocate a vector for storing results in the original order
-    ces = Vector{CounterfactualExplanations.AbstractCounterfactualExplanation}(undef, length(args))
+    return_flattened = get(kwargs, :return_flattened, false)
+    if return_flattened
+        ces = Vector{CounterfactualExplanations.FlattenedCE}(undef, length(args))
+    else
+        ces = Vector{CounterfactualExplanations.CounterfactualExplanation}(undef, length(args))
+    end
 
     # Verbosity setup:
     if verbose
