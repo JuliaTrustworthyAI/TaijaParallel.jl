4127dc901b1012881a779d51844d2201a8db85c8
pat-alt <altmeyerpat@gmail.com>
2025-01-08 11:05:32 +0100

    this should be sorted now

--- test/CounterfactualExplanations.jl/mpi.jl
+++ test/CounterfactualExplanations.jl/mpi.jl
@@ -1,5 +1,9 @@
+using CounterfactualExplanations
+using CounterfactualExplanations: counterfactual
 using CounterfactualExplanations.DataPreprocessing: CounterfactualData
+using CounterfactualExplanations.Convergence
 using CounterfactualExplanations.Evaluation: benchmark
+using CounterfactualExplanations.Models
 using Logging
 using TaijaData
 using TaijaParallel
@@ -8,12 +12,42 @@ using Test
 # Initialize MPI
 using MPI
 MPI.Init()
+parallelizer = TaijaParallel.MPIParallelizer(MPI.COMM_WORLD)
 
 data = TaijaData.load_linearly_separable()
 counterfactual_data = CounterfactualData(data[1], data[2])
-parallelizer = TaijaParallel.MPIParallelizer(MPI.COMM_WORLD)
+
+# Select factuals:
+M = fit_model(counterfactual_data, :MLP)
+conv = MaxIterConvergence(10)
+generator = GenericGenerator()
+factual = 1
+target = 2
+chosen = rand(findall(predict_label(M, counterfactual_data) .== factual), 1000)
+xs = select_factual(counterfactual_data, chosen)
+target = fill(2, length(xs))
+
+ces = TaijaParallel.parallelize(
+    parallelizer,
+    CounterfactualExplanations.generate_counterfactual,
+    xs, 
+    target, 
+    counterfactual_data, 
+    M, 
+    generator; 
+    convergence=conv, 
+    initialization=:identity,
+)
+
+nsteps = (ce -> total_steps(ce)).(ces)
+if MPI.Comm_rank(MPI.COMM_WORLD) == 0
+    println("Total steps: ", nsteps)
+end
+@test allequal(nsteps)
+
+# Benchmark CE with MPI
 with_logger(NullLogger()) do
-    bmk = benchmark(counterfactual_data; parallelizer = parallelizer)
+    bmk = benchmark(counterfactual_data; parallelizer=parallelizer)
 end
 MPI.Finalize()
 @test MPI.Finalized()
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,13 @@ using Test
 
 @testset "TaijaParallel.jl" begin
 
-    @testset "Quality Assurance" begin
-        include("aqua.jl")
-    end
+    # @testset "Quality Assurance" begin
+    #     include("aqua.jl")
+    # end
 
-    @testset "Traits" begin
-        include("traits.jl")
-    end
+    # @testset "Traits" begin
+    #     include("traits.jl")
+    # end
 
     @testset "CounterfactualExplanations.jl" begin
         include("CounterfactualExplanations.jl/CounterfactualExplanations.jl")
